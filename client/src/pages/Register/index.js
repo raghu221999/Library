d591895019b90371c89e4f0093492dcf4266c1d7
Raghavendra B <raghu221999@gmail.com>
2023-06-04 18:22:52 +0530

    first commit

---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Form } from 'antd';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
+
+function Register() {
+
+  return (
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="authentication-form bg-white p-3 rounded">
+        <h1 className="text-secondary text-2xl font-bold mb-1">
+          REGISTER
+        </h1>
+        <hr />
+        <Form layout="vertical">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name!",
+              },
+            ]}
+          >
+            <input type="text" placeholder="Name" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <input type="email" placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            label="Phone Number"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+            ]}
+          >
+            <input type="number" placeholder="Phone Number" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <input type="password" placeholder="Password" />
+          </Form.Item>
+
+          <div className="text-center mt-2 flex flex-col gap-1">
+            <Button title="Register" type="submit" />
+            <Link to="/login" className="text-primary text-sm underline">
+              Already have an account? Click Here To Login
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
